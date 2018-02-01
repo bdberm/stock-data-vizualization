646b65483adde417ed7fafd4c99a5f784e76333f
Benjamin Berman <bdberm@gmail.com>
2018-02-01 17:19:43 -0500

    first api call

--- lib/entry.jsx
+++ lib/entry.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import {fetchSymbols} from './util/iex_api_util';
 
 document.addEventListener("DOMContentLoaded",() => {
+  window.fetchSymbols = fetchSymbols;
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root />, root);
 });
