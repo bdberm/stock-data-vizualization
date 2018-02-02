f1acf9dca6f8232f9a4c102db1654ed18155428b
Benjamin Berman <bdberm@gmail.com>
2018-02-02 09:59:44 -0500

    make symbols slice of state normalized object with symbol as key

---
+++ lib/components/search/search_container.jsx
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+import Search from './search';
+import {fetchSymbols} from '../actions/symbol_actions';
+
+const mapStateToProps = (state) => (
+  {
+    symbols: state.symbols
+  }
+);
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    fetchSymbols: (() => dispatch(fetchSymbols()))
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
