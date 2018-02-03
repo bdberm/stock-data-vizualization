079b958a201aa5d762be20597091e4c8ef179226
Benjamin Berman <bdberm@gmail.com>
2018-02-03 11:37:10 -0500

    properly fetching quote data on mounting of stock show

--- lib/components/stock_info/stock_info.jsx
+++ lib/components/stock_info/stock_info.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 class StockInfo extends React.Component {
 
+  componentDidMount() {
+    this.props.fetchQuote(this.props.match.params.symbol);
+  }
+
+  componentWillReceiveProps(newProps) {
+    const newSym = newProps.match.params.symbol;
+
+    if (newSym !== this.props.match.params.symbol) {
+      this.props.fetchQuote(newSym);
+    }
+  }
+
   render() {
     const {stock} = this.props;
     return (
